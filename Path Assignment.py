69f919e7c7bf68d812ad8a7348011be715010fe2
bansalapoorv29sb <112248370+bansalapoorv29sb@users.noreply.github.com>
2024-06-10 10:26:05 +0530

    Create Path Assignment.py

---
+++ Path Assignment.py
@@ -0,0 +1,8 @@
+data_dir = '../input/gtsrb-german-traffic-sign'
+train_path = '../input/gtsrb-german-traffic-sign/Train'
+test_path = '../input/gtsrb-german-traffic-sign/'
+
+# Resizing the images to 30x30x3
+IMG_HEIGHT = 30
+IMG_WIDTH = 30
+channels = 3
