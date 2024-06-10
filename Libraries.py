0cdfc0aad29b80361a99c221f484651b9d797872
bansalapoorv29sb <112248370+bansalapoorv29sb@users.noreply.github.com>
2024-06-10 10:25:22 +0530

    Create Libraries.py

---
+++ Libraries.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import os
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import accuracy_score
+np.random.seed(42)
+
+from matplotlib import style
+style.use('fivethirtyeight')
