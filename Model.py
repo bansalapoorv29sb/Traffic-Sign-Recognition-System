a05b0e223cdfc3fec404d63630df50001c1ef3cf
bansalapoorv29sb <112248370+bansalapoorv29sb@users.noreply.github.com>
2024-06-10 10:35:50 +0530

    Create Model.py

---
+++ Model.py
@@ -0,0 +1,27 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Flatten, Dense, Dropout, GlobalAveragePooling2D
+
+model = Sequential([    
+    Conv2D(filters=16, kernel_size=(3, 3), activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, channels)),
+    Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    BatchNormalization(axis=-1),
+    
+    Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    Conv2D(filters=128, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    BatchNormalization(axis=-1),
+    
+    Conv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same'),
+    Conv2D(filters=512, kernel_size=(3, 3), activation='relu', padding='same'),
+    MaxPooling2D(pool_size=(2, 2)),
+    BatchNormalization(axis=-1),
+
+    GlobalAveragePooling2D(),
+
+    Dense(512, activation='relu'),
+    BatchNormalization(),
+    Dropout(rate=0.5),
+    
+    Dense(43, activation='softmax')
+])
