ad67d181d1314e4b26ad61b9f427fbe3798ae875
bansalapoorv29sb <112248370+bansalapoorv29sb@users.noreply.github.com>
2024-06-10 10:28:49 +0530

    Create Visualisation.py

---
+++ Visualisation.py
@@ -0,0 +1,21 @@
+folders = os.listdir(train_path)
+
+train_number = []
+class_num = []
+
+for folder in folders:
+    train_files = os.listdir(train_path + '/' + folder)
+    train_number.append(len(train_files))
+    class_num.append(classes[int(folder)])
+    
+# Sorting the dataset on the basis of number of images in each class
+zipped_lists = zip(train_number, class_num)
+sorted_pairs = sorted(zipped_lists)
+tuples = zip(*sorted_pairs)
+train_number, class_num = [ list(tuple) for tuple in  tuples]
+
+# Plotting the number of images in each class
+plt.figure(figsize=(21,10))  
+plt.bar(class_num, train_number)
+plt.xticks(class_num, rotation='vertical')
+plt.show()
