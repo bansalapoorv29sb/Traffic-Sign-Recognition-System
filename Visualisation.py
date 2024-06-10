8e5b6fedb6cd51f1630094d1c63ac0fb25474e4c
bansalapoorv29sb <112248370+bansalapoorv29sb@users.noreply.github.com>
2024-06-10 10:32:09 +0530

    Update Visualisation.py

--- Visualisation.py
+++ Visualisation.py
@@ -19,3 +19,23 @@ plt.figure(figsize=(21,10))
 plt.bar(class_num, train_number)
 plt.xticks(class_num, rotation='vertical')
 plt.show()
+
+
+
+# Visualizing 25 random images from test data
+import random
+from matplotlib.image import imread
+
+test = pd.read_csv(data_dir + '/Test.csv')
+imgs = test["Path"].values
+
+plt.figure(figsize=(25,25))
+
+for i in range(1,26):
+    plt.subplot(5,5,i)
+    random_img_path = data_dir + '/' + random.choice(imgs)
+    rand_img = imread(random_img_path)
+    plt.imshow(rand_img)
+    plt.grid(b=None)
+    plt.xlabel(rand_img.shape[1], fontsize = 20)#width of image
+    plt.ylabel(rand_img.shape[0], fontsize = 20)#height of image
