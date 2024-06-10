42051251cbe81ec4ba6e6d61111622cf7e814937
bansalapoorv29sb <112248370+bansalapoorv29sb@users.noreply.github.com>
2024-06-10 10:40:21 +0530

    Update TrainningModel.py

--- TrainningModel.py
+++ TrainningModel.py
@@ -1,1 +1,11 @@
+aug = ImageDataGenerator(
+    rotation_range=10,
+    zoom_range=0.15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.15,
+    horizontal_flip=False,
+    vertical_flip=False,
+    fill_mode="nearest")
 
+history = model.fit(aug.flow(X_train, y_train, batch_size=32), epochs=epochs, validation_data=(X_val, y_val))
